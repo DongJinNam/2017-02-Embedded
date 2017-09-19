29ada50dc12bd782dc795a5b9d4650473c19dd53
DESKTOP-8H727PG\NDJ <ehdwls6856@gmail.com>
2017-09-20 01:41:01 +0900

    170913일자 ucos2 운영체제 내에서 Multitasking 실습

---
+++ multitasking/exercise4/EX1.C
@@ -0,0 +1,270 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                           (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                                           All Rights Reserved
+*
+*                                               EXAMPLE #1
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+
+/*
+*********************************************************************************************************
+*                                               CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  TASK_STK_SIZE                 512       /* Size of each task's stacks (# of WORDs)            */
+#define  N_TASKS                        4       /* Number of identical tasks                          */
+
+/*
+*********************************************************************************************************
+*                                               VARIABLES
+*********************************************************************************************************
+*/
+
+OS_STK        TaskStk[N_TASKS][TASK_STK_SIZE];        /* Tasks stacks                                  */
+OS_STK        TaskStartStk[TASK_STK_SIZE];
+char          TaskData[N_TASKS];                      /* Parameters to pass to each task               */
+OS_EVENT     *PrintSem; // 색상 출력 시에 사용할 세마포어
+INT8U		 PrioMap[N_TASKS*4] = {1,2,3,4,6,7,8,5,3,4,1,2,8,5,6,7}; // 4개 씩 끊어서 각 task 우선순위를 변경합니다.
+
+/*
+*********************************************************************************************************
+*                                           FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/		
+        void  Task(void *data);                       /* Function prototypes of tasks                  */
+        void  TaskStart(void *data);                  /* Function prototypes of Startup task           */
+static  void  TaskStartCreateTasks(void);
+static  void  TaskStartDispInit(void);
+static  void  TaskStartDisp(void);
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                                MAIN
+*********************************************************************************************************
+*/
+
+void  main (void)
+{
+    PC_DispClrScr(DISP_FGND_WHITE + DISP_BGND_BLACK);      /* Clear the screen                         */
+
+    OSInit();                                              /* Initialize uC/OS-II                      */
+
+    PC_DOSSaveReturn();                                    /* Save environment to return to DOS        */
+    PC_VectSet(uCOS, OSCtxSw);                             /* Install uC/OS-II's context switch vector */
+
+    PrintSem = OSSemCreate(1);                          /* 색상 출력 시 사용할 세마포어                  */
+
+    OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], 0);
+    OSStart();                                             /* Start multitasking                       */
+}
+
+
+/*
+*********************************************************************************************************
+*                                              STARTUP TASK
+*********************************************************************************************************
+*/
+void  TaskStart (void *pdata)
+{
+#if OS_CRITICAL_METHOD == 3                                /* Allocate storage for CPU status register */
+    OS_CPU_SR  cpu_sr;
+#endif
+    char       s[100];
+    INT16S     key;
+	INT8U i, cnt = 0;
+	INT8U b = 0, c = 0;
+
+    pdata = pdata;                                         /* Prevent compiler warning                 */
+
+    TaskStartDispInit();                                   /* Initialize the display                   */
+
+    OS_ENTER_CRITICAL();
+    PC_VectSet(0x08, OSTickISR);                           /* Install uC/OS-II's clock tick ISR        */
+    PC_SetTickRate(OS_TICKS_PER_SEC);                      /* Reprogram tick rate                      */
+    OS_EXIT_CRITICAL();
+
+    OSStatInit();                                          /* Initialize uC/OS-II's statistics         */
+		
+    TaskStartCreateTasks();                                /* Create all the application tasks         */
+	
+	// 4번 상황에서는 중지 하는 테스크가 없습니다.
+	
+    for (;;) {				
+        TaskStartDisp();                                  /* Update the display                       */		
+		
+		if (cnt == 3) { // 우선순위를 바꿀 때			
+			c = b+1;
+			if (c > 3) c = 0;			
+			// 동적 우선순위 변경. 우선순위는 PrioMap 순서에 따라서 변경하도록 합니다.
+			for (i = 0; i < 4; i++) {
+				OSTaskChangePrio(PrioMap[b*4+i],PrioMap[c*4+i]);
+			}			
+			b++;
+			if (b > 3) b = 0;
+		}
+																		
+        if (PC_GetKey(&key) == TRUE) {                     /* See if key has been pressed              */
+            if (key == 0x1B) {                             /* Yes, see if it's the ESCAPE key          */
+                PC_DOSReturn();                            /* Return to DOS                            */
+            }
+        }		
+		cnt++; // loop iterator
+		if (cnt > 3) cnt = 0;
+		
+        OSCtxSwCtr = 0;                                    /* Clear context switch counter             */
+        OSTimeDlyHMSM(0, 0, 1, 0);                         /* Wait one second                          */
+    }
+}
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                        INITIALIZE THE DISPLAY
+*********************************************************************************************************
+*/
+
+static  void  TaskStartDispInit (void)
+{
+/*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
+/*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
+    PC_DispStr( 0,  0, "                         uC/OS-II, The Real-Time Kernel                         ", DISP_FGND_WHITE + DISP_BGND_RED + DISP_BLINK);
+    PC_DispStr( 0,  1, "                                Jean J. Labrosse                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  2, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  3, "                                    EXAMPLE #1                                  ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  4, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  5, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  6, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  7, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  8, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  9, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 10, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 11, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 12, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 13, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 14, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 15, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 16, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 17, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 18, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 19, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 20, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 21, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 22, "#Tasks          :        CPU Usage:     %                                       ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 23, "#Task switch/sec:                                                               ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0, 24, "                            <-PRESS 'ESC' TO QUIT->                             ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY + DISP_BLINK);
+/*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
+/*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
+}
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                           UPDATE THE DISPLAY
+*********************************************************************************************************
+*/
+
+static  void  TaskStartDisp (void)
+{
+    char   s[80];
+
+
+    sprintf(s, "%5d", OSTaskCtr);                                  /* Display #tasks running               */
+    PC_DispStr(18, 22, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
+
+#if OS_TASK_STAT_EN > 0
+    sprintf(s, "%3d", OSCPUUsage);                                 /* Display CPU usage in %               */
+    PC_DispStr(36, 22, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
+#endif
+
+    sprintf(s, "%5d", OSCtxSwCtr);                                 /* Display #context switches per second */
+    PC_DispStr(18, 23, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
+
+    sprintf(s, "V%1d.%02d", OSVersion() / 100, OSVersion() % 100); /* Display uC/OS-II's version number    */
+    PC_DispStr(75, 24, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
+
+    switch (_8087) {                                               /* Display whether FPU present          */
+        case 0:
+             PC_DispStr(71, 22, " NO  FPU ", DISP_FGND_YELLOW + DISP_BGND_BLUE);
+             break;
+
+        case 1:
+             PC_DispStr(71, 22, " 8087 FPU", DISP_FGND_YELLOW + DISP_BGND_BLUE);
+             break;
+
+        case 2:
+             PC_DispStr(71, 22, "80287 FPU", DISP_FGND_YELLOW + DISP_BGND_BLUE);
+             break;
+
+        case 3:
+             PC_DispStr(71, 22, "80387 FPU", DISP_FGND_YELLOW + DISP_BGND_BLUE);
+             break;
+    }
+}
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                             CREATE TASKS
+*********************************************************************************************************
+*/
+
+static  void  TaskStartCreateTasks (void)
+{
+    INT8U  i;
+    for (i = 0; i < N_TASKS; i++) { /* Create N_TASKS identical tasks           */
+		TaskData[i] = '0' + i;
+		// 1,2,3,4번 테스크 모두 생성 (초반에는 1번 테스크가 우선순위 1, 2번 테스크가 우선순위 2, 3번 테스크가 우선순위 3, 4번 테스크가 우선순위 4를 가지도록 합니다)
+		OSTaskCreate(Task, (void *)&TaskData[i], &TaskStk[i][TASK_STK_SIZE - 1], i+1);				
+    }
+}
+
+/*
+*********************************************************************************************************
+*                                                  TASKS
+*********************************************************************************************************
+*/
+
+void  Task (void *pdata)
+{
+    INT8U  x;
+    INT8U  y;
+    INT8U  err;
+	INT8U  i;
+
+    for (;;) {			
+		char *data = (char *)pdata;				
+		OSSemPend(PrintSem,0,&err);		
+		if (*data == '0') { // Red 출력 (1번 테스크)
+			for (i = 5; i < 21; i++) {
+				PC_DispStr( 0,  i, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_RED);
+			}						
+		}
+		else if (*data == '1') { // Blue 출력	(2번 테스크)						
+			for (i = 5; i < 21; i++) {
+				PC_DispStr( 0,  i, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_BLUE);
+			}			
+		}
+		else if (*data == '2') { // Brown 출력 (3번 테스크)
+			for (i = 5; i < 21; i++) {
+				PC_DispStr( 0,  i, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_BROWN);
+			}
+		}		
+		else if (*data == '3') { // Green 출력 (4번 테스크)
+			for (i = 5; i < 21; i++) {
+				PC_DispStr( 0,  i, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_GREEN);
+			}					
+		}				
+		OSTimeDlyHMSM(0,0,2,0);	
+		OSSemPost(PrintSem);
+    }
+}
+
+
